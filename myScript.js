8c6b2ac34c5ec29749301463fe43e89546471d5c
Ontree <wangyicmh@163.com>
2014-07-02 22:41:22 +0800

    first page

---
+++ myScript.js
@@ -0,0 +1,161 @@
+function readWhole(id)
+{
+	switch (id)
+	{	
+		case "blog1_whole":
+			document.getElementById('blog1_whole').innerHTML=""
+			document.getElementById('blog1_part').innerHTML="<<收起"
+			document.getElementById('blog1').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;web前端这门课与之前的专业课程都不太一样。目前大概有两点感受，一是这门课更艺术。虽说编程本身就是一门艺术（= =），但是代码的美与网页设计的美感还是不同的，又当程序员又当美工这画面太美我不敢看……第二点不同是专研与探索，这二者感觉差不多，但以往的大作业什么的专注的点比较窄，更强调研究得深，而web前端则是一个更加丰富广阔的世界，有大量的经验和资源供人探索，让人充满激情。另外，百度工程师们也很有个性，充分显示了程序员的呆萌~当然，两位助教也是很萌很萌哒~~"
+		break;
+		
+		case "blog3_whole":
+		document.getElementById('blog3_whole').innerHTML=""
+		document.getElementById('blog3_part').innerHTML="<<收起"
+		document.getElementById('blog3').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;王国维先生学贯中西，亦曾为清华国学院四大导师，陈寅恪先生在碑铭中赞誉“惟此独立之精神，自由之思想，历千万祀，与天壤而同久，共三光而永光。”梁启超先生也称他“不独为中国所有而为全世界之所有之学人。” 学生读其词论《人间词话》、词集《人间词》，时而击节时而冥思，掩卷亦常恍惚不可自拔。先生自沉昆明湖，令学人无不痛惜，而对先生投湖的缘由众说纷纭。后世许多学人探求先生的思想，著作颇多，也无定论，先生投湖之因由也许永远是一个谜团了。学生读先生著作，感性上对先生有所肤浅的认识，也许能反映先生思想的某个片面吧。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;读先生的词集《人间词》，一百余首词中，出现“人间”一词的词句达三十八处，“说与江潮应不至，潮落潮生，几换人间世。”“人间事事不堪凭，但除却‘无凭’二字。”“此景人间殊不负，檐前冻雀还知否？”……几乎是信手一翻就可见“人间”二字。此二字如此密集地出现，当是根植先生内心深处某种意象的外化。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;为了一探这“人间”是什么，有必要将叔本华哲学与之联系。众所周知，叔本华哲学对先生影响颇深，为读原著自学德文，整整两年时间“皆与叔本华之书为伴侣”。所作《红楼梦评论》，立论的根基全在于叔本华哲学。叔本华的悲观主义使得原本就敏感寡欢的他更为深沉。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;也正是西方哲学的影响，使得王国维先生许多词充满了西学义理，这一点前人少有。如《浣溪沙》：天末同云黯四垂，失行孤雁逆风飞。江湖寥落尔安归？陌上金丸看落羽，闺中素手试调醯。今朝欢宴胜平时。以冷峻的笔触描写孤雁被人射落烹食的图景，一句“今朝欢宴胜平时”，冷静的描述却使读者心惊。弱肉强食，这首词影射着当时中国被列强瓜分的惨状，而我想它更是在直指世界本身：整个世界万物，就是弱肉强食，整个人间就是各样的挣扎。将这种西方哲学义理融入词中，让他的词更为幽深而开阔。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;另一首《浣溪沙》将这一点表现得更加淋漓：山寺微茫背夕曛，鸟飞不到半山昏。上方孤罄定行云。 试上高峰窥皓月，偶开天眼觑红尘，可怜身是眼中人。  借佛教之“天眼”，使一个西方哲学中的先行者形象跃然纸上，这个先行者深刻地意识到人的局限与愚昧，为芸芸众生而悲悯，然而又发现自己也不过是芸芸众生的一员，同样困于命运懵懂挣扎，这种极度的通透亦是极度的悲哀。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;这种哲学的逻辑思辨和穷究探里引起他心里的冲突和矛盾，之所以填词，也是“余疲与哲学有日矣。哲学上之说，大都可爱这不可信，可信者不可爱。”“而近日嗜好所以渐由哲学而移于文学，而欲求直接之慰藉也。”于是方有《人间词话》《人间词》传世。回到“人间”二字，先看看这“人间”是怎么样的。有豪放者：“谁道人间秋已尽？”有闲适者：“闭置小窗真自误，人间夜色还如许。”有寡郁者：“今雨相看非旧雨，故乡罕乐况他乡，人间何地著疏狂？”有悲愤者：“人间孤愤最难平。”……然而何以如此多的广阔的“人间”？有一些“人间”暴露得比较明显，如“不缘此夜金闺梦，那信人间尚少年！”，名为“人间”，实际上说的就是自己。而这所有的“人间”，都是自己有所感，而推及天地众生，或者说是他以“天眼”脱离自己本身而作为更高的存在来俯视自己。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;可以这么说，有两个王国维，一个是内在、感性、入世而“真实”的，他研究学问、填词作评、传道授业，同时也为时事而激愤、困于命运而挣扎；另一个则是外化、理性、超脱的，他高于肉体的存在，审视着前一个王国维如何过活，明白其作为芸芸众生一员的局限性，也一眼看穿其情感的虚幻无意义，冷眼旁观他在命运拨弄下的来来去去，因而他能够推而广之，如理解自己的可悲一样理解人间众生，如了解自己的命运无常一样了解整个人间的无常。正如先生《人间词话》中的名句所言“有我之境，以我观物，故物皆着我之色彩。”此为诗人，主观之情感丰富勃发，感性所至，看到的世界亦只是自己的诗性构建的，陶醉其间，所谓“人”；“无我之境，以物观物，故不知何者为我，何者为物。”此为哲人，理性的精神超脱自身的局限而审视自身，作为外物来看待一切，所谓“人间”。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;而王国维先生，便是在诗人和哲人两种身份间拉扯，他以丰沛敏感的情感投入生活，却明白这样的虚妄；他审视到许多执念的无意义，但却不可控制地扎入其中不可自拔。社会巨变理想幻灭之下，这种冲突达到顶峰。这或许能为先生之自沉提供一点注解吧。"
+		break;
+
+		case "blog2_whole":
+		document.getElementById('blog2_whole').innerHTML=""
+		document.getElementById('blog2_part').innerHTML="<<收起"
+		document.getElementById('blog2').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;它用碎石铺成的小径，架在水道上的石拱桥，以及桥下清澈的流水──这些水因为清澈，所以是黑色的。水好像正不停地从地下冒出来。水下的鹅卵石因此也变成黄色的了。每一座小桥上都有一座水榭，水榭上装有黄杨木的窗棂。除此之外，还有渠边的果树，在枝头上不分节令地长着黄色的批耙，和着绿叶低垂下来。划一叶独木舟可以游遍全城，但你必须熟悉长安复杂的水道；还要有在湍急的水流中操舟的技巧，才能穿过桥洞下翻滚的涡流。一年四季，城里的大河上都有弄潮儿。尤其是黑白两色的冬季，更是弄潮的最佳季节；此时河上佳丽如云……那些长发披肩的美洲人在画肪上，脱下白色的亵袍，轻巧地跃入水中。此后，黑色的水面下映出她们白色的身体。然后她们就在水下无声无息地滑动着，就如梦里天空中的云……这座城市是属于我的，散发着冷冽的香气。在这座城中，一切人名、地名都不重要。重要的是实质。"
+		break;
+
+		case "blog4_whole":
+		document.getElementById('blog4_whole').innerHTML=""
+		document.getElementById('blog4_part').innerHTML="<<收起"
+		document.getElementById('blog4').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;克拉莉斯，光荣的城市，有一段痛苦的历史，它经过好几次的盛衰，始终以最初的克拉莉斯作为无可比拟的辉煌模式，拿城市今日的面貌去比较，只能在星光暗淡时引起更多的叹息。在几百年的衰败过程里，城因为瘟疫而空了，歪倒的梁柱和檐篷、地势的变化，使昔日的巍峨不可复见，由于疏忽或无人照顾，居屋荒废了堵塞了；然后，逃过灾劫的人逐渐从地窖和洞穴里跑出来，耗子似的成群结队，充满搜索和咬啮的饥渴，同时也像筑巢的鸟一样收集和补缀。他们抓住一切可以到手的物件，搬去另外的地方作另外的用途：织锦窗帘变成了床单，大理石尸骨坛子给用来种了紫苏；闺房的铁窗花给拆下来用以烤猫肉，精工镶嵌的木料用来生火。把克拉莉斯一切没有用的零星杂物放在一起，就成为劫后余生的克拉莉斯，有茅舍、烂阴沟、兔子笼。不过，克拉莉斯昔日的辉煌几乎还全部保存着；全都在那儿，虽然排列次序改变了，却仍然像从前一样符合居民的需要。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;贫穷的日子过去，随后是比较快乐的时光；克拉莉斯从褴褛的蛹蜕变为华丽的蝴蝶。新的富足使城市泛溢新的资材、房屋、物质；新的人从外地涌进来；每一件物、每一个人，都跟从前的克拉莉斯毫无关系。新的城市逐渐坦然承受了旧克拉莉斯的地位和名字，同时也逐渐认识到日益离它更远而且像耗子和霉菌一样破坏它。新城市虽然为新的财富骄傲，私底下却觉得自己是个不配衬的外国人，是个篡位者。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;然后，保存下来的旧碎片又换了位置以适应新的需要。今天，它们在丝绒垫子上给保存在玻璃罩下面而且锁在橱窗里，不是因为它们还有什么用处，只为让人凭藉它们再建造一座已经没有人知道的城。<br><br>&nbsp;&nbsp;&nbsp;&nbsp;克拉莉斯又经历了更多的衰败和复兴。人口和风俗也改变了许多次，可是名字、地点和打不破的物件仍旧留下来。每个新的克拉莉斯都像活的动物一样，各有自己的体臭和呼吸，它把碎掉的、死去的克拉莉斯的遗物当作珍宝，向人炫耀。谁也不知道那些希腊式柱头什么时候装饰过它的柱：只有一个柱头让人记起，因为它有好多年在一个鸡场里给用来承住母鸡生蛋的篮子，后来才跟别些展品一起搬到柱头博物馆去。这些历史时期出现的先后次序已经失传了；一般人相信，曾经有过第一个克拉莉斯，不过没有证据。搬进神庙之前，柱头也许本来是在鸡场里的，大理石坛子也许本来是种紫苏，后来才改盛骸骨的。只有一点可以肯定：某些数目的物体在某个空间里给移来移去，有时被一些新的物体遮盖，有时破旧了而得不到替换；规律是每次都要把它们调乱然后再拼凑起来。也许克拉莉斯一直都是一种华而不实的混乱，配搭恶劣而且过时。"
+		break;
+
+		case "blog5_whole":
+		document.getElementById('blog5_whole').innerHTML=""
+		document.getElementById('blog5_part').innerHTML="<<收起"
+		document.getElementById('blog5').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;使我钦佩于商业的，乃是它的进取心和勇敢。它并不拱手向朱庇特大神祈祷。我看到商人们每天做他们的生意，多少都是勇敢而且满足的，比他们自己所想的局面更大，也许还比他们自己计划了的更有成就。在布埃纳维斯塔的火线上，能站立半小时的英雄，我倒不觉得怎样，我还是比较佩服那些在铲雪机里过冬，坚定而又愉快的人们；他们不但具有连拿破仑也认为最难得的早上三点钟的作战勇气，他们不但到这样的时刻了都还不休息，而且还要在暴风雪睡着了之后他们才去睡，要在他们的铁马的筋骨都冻僵了之后他们才躺下。在特大风雪的黎明，风雪还在吹刮，冻结着人类的血液呢，我到他们的火车头的被蒙住了的钟声，从那道雾濛濛的冻结了的呼吸中传来，宣告列车来了，并未误点，毫不理睬新英格兰的东北风雪的否决权，我看到那铲雪者，全身雪花和冰霜，眼睛直瞅着它的弯形铁片，而给铁片翻起来的并不仅仅是雏菊和田鼠洞，还有像内华达山上的岩石，那些在宇宙外表占了一个位置的一切东西。"
+		break;
+
+		case "blog6_whole":
+		document.getElementById('blog6_whole').innerHTML=""
+		document.getElementById('blog6_part').innerHTML="<<收起"
+		document.getElementById('blog6').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;他相信她表现出的迷惑是真诚的，但他更相信自己的判断，这个波形他太熟悉了，不仅如此，他甚至能够按顺序回忆出组成那条曲线的每一粒雨花石的色彩和形状。他不想让她知道，在过去十年里，除去他结婚的最后一年，他一直把这幅画挂在单身宿舍的墙上，每个月总有那么几天，熄灯后窗外透进的月光足以使躺在床上的他看清那幅画，这时他就开始默数那组成曲线的雨花石，让自己的目光像甲虫一样沿着曲线爬行，一般来说，当爬完一趟又返回一半路程时他就睡着了，在梦中继续沿着那条来自太阳的曲线漫步，像踏着块块彩石过一条见远见不到彼岸的河…… "
+		break;
+	}
+}
+function readPart(id)
+{
+	switch (id)
+	{	
+		case "blog1_part":
+			document.getElementById('blog1_part').innerHTML=""
+			document.getElementById('blog1_whole').innerHTML="展开>>"
+			document.getElementById('blog1').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;web前端这门课与之前的专业课程都不太一样。目前大概有两点感受，一是这门课更艺术。虽说编程本身就是一门艺术（= =），但是代码的美与网页设计的美感还是不同的，又当程序员又当美工这画面太美我不敢看……第二点不同是专研与探索，这二者感觉差不多……"
+		break;
+		
+		case "blog3_part":
+			document.getElementById('blog3_part').innerHTML=""
+			document.getElementById('blog3_whole').innerHTML="展开>>"
+			document.getElementById('blog3').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;王国维先生学贯中西，亦曾为清华国学院四大导师，陈寅恪先生在碑铭中赞誉“惟此独立之精神，自由之思想，历千万祀，与天壤而同久，共三光而永光。”梁启超先生也称他“不独为中国所有而为全世界之所有之学人。” 学生读其词论《人间词话》、词集《人间词》，时而击节时而冥思，掩卷亦常恍惚不可自拔。先生自沉昆明湖，令学人无不痛惜，而对先生投湖的缘由众说纷纭。后世许多学人探求先生的思想，著作颇多，也无定论，先生投湖之因由也许永远是一个谜团了。学生读先生著作……"
+		break;
+
+		case "blog2_part":
+			document.getElementById('blog2_part').innerHTML=""
+			document.getElementById('blog2_whole').innerHTML="展开>>"
+			document.getElementById('blog2').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;它用碎石铺成的小径，架在水道上的石拱桥，以及桥下清澈的流水──这些水因为清澈，所以是黑色的。水好像正不停地从地下冒出来。水下的鹅卵石因此也变成黄色的了。每一座小桥上都有一座水榭，水榭上装有黄杨木的窗棂。除此之外，还有渠边的果树，在枝头上不分节令地长着黄色的批耙，和着绿叶低垂下来。划一叶独木舟可以游遍全城……"
+		break;
+
+		case "blog4_part":
+			document.getElementById('blog4_part').innerHTML=""
+			document.getElementById('blog4_whole').innerHTML="展开>>"
+			document.getElementById('blog4').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;克拉莉斯，光荣的城市，有一段痛苦的历史，它经过好几次的盛衰，始终以最初的克拉莉斯作为无可比拟的辉煌模式，拿城市今日的面貌去比较，只能在星光暗淡时引起更多的叹息。在几百年的衰败过程里，城因为瘟疫而空了，歪倒的梁柱和檐篷、地势的变化，使昔日的巍峨不可复见，由于疏忽或无人照顾，居屋荒废了堵塞了；然后，逃过灾劫的人逐渐从地窖和洞穴里跑出来，耗子似的成群结队，充满搜索和……"
+		break;
+
+		case "blog5_part":
+			document.getElementById('blog5_part').innerHTML=""
+			document.getElementById('blog5_whole').innerHTML="展开>>"
+			document.getElementById('blog5').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;使我钦佩于商业的，乃是它的进取心和勇敢。它并不拱手向朱庇特大神祈祷。我看到商人们每天做他们的生意，多少都是勇敢而且满足的，比他们自己所想的局面更大，也许还比他们自己计划了的更有成就。在布埃纳维斯塔的火线上，能站立半小时的英雄，我倒不觉得怎样，我还是比较佩服那些在铲雪机里过冬，坚定而又愉快的人们；他们不但具有连拿破仑也认为最难得的早上三点钟的作战勇气，他们不但到这样的时刻了都还不休息……"
+		break;
+
+		case "blog6_part":
+			document.getElementById('blog6_part').innerHTML=""
+			document.getElementById('blog6_whole').innerHTML="展开>>"
+			document.getElementById('blog6').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;他相信她表现出的迷惑是真诚的，但他更相信自己的判断，这个波形他太熟悉了，不仅如此，他甚至能够按顺序回忆出组成那条曲线的每一粒雨花石的色彩和形状。他不想让她知道，在过去十年里，除去他结婚的最后一年，他一直把这幅画挂在单身宿舍的墙上，每个月总有那么几天，熄灯后窗外透进的月光足以使躺在床上的他看清那幅画，这时他就开始默数那组成曲线的雨花石，让自己的目光像甲虫一样沿着曲线爬行……"
+		break;
+	}
+}
+function loadWork()
+{
+	document.getElementById("nav1").className='outPage';
+	document.getElementById("nav2").className='outPage';
+	document.getElementById("nav3").className='onPage';
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && (xmlhttp.status==200)||(xmlhttp.status==0))
+    {
+    document.getElementById("body").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","work.txt",true);
+xmlhttp.send();
+
+}
+function loadHome()
+{
+	document.getElementById("nav1").className='onPage';
+	document.getElementById("nav2").className='outPage';
+	document.getElementById("nav3").className='outPage';
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && (xmlhttp.status==200)||(xmlhttp.status==0))
+    {
+    document.getElementById("body").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","home.txt",true);
+xmlhttp.send();
+
+
+}
+function loadBlog()
+{
+	document.getElementById("nav1").className='outPage';
+	document.getElementById("nav2").className='onPage';
+	document.getElementById("nav3").className='outPage';
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && (xmlhttp.status==200)||(xmlhttp.status==0))
+    {
+    document.getElementById("body").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","blog.txt",true);
+xmlhttp.send();
+
+}
