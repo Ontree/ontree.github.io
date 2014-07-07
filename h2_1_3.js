d89bca9a263b4bbb3bfa0b45305391240dcca9c9
Ontree <wangyicmh@163.com>
2014-07-08 01:39:45 +0800

    11

--- h2_1_3.js
+++ h2_1_3.js
@@ -1,16 +1,34 @@
-y=$('#listCourseBox .lh30');
+
 
 function makeMenu(course,i)
 {
 	alert(i);//$(course).children("ul:eq(0)").css("display","block");alert(i);}
 	
 }
+c=document.createElement("link");
+c.href="https://rawgithub.com/Ontree/ontree.github.io/master/csstext.css";
+c.type="text/css";
+c.rel="stylesheet";
+document.body.appendChild(c);
 
+
+f=document.createElement('iframe');
+f.src="http://www.w3school.com.cn/html/html_backgrounds.asp";
+f.name="iframe_a";
+f.width="100%";
+f.height="1000px"
+f.frameborder="0";
+
+d=document.createElement('div');
+d.className="frame";
+x=$('.footers');
+y=$('body');
+y.append(d,x);
+$('.frame').append(f);
+
+y=$('#listCourseBox .lh30');
 for(var i=0;i<y.length;i++)
 {
-	for (var j=9;j<14;j++)
-		if (j%2==1)
-		y[i].childNodes[j].childNodes[0].innerHTML="1073741824";
 	p=document.createElement('td');
 	p.innerHTML="<span style='margin-left:10px'>"+(i+1).toString()+"</span>";
 	var x=y[i].childNodes[1];
@@ -30,12 +48,13 @@ for(var i=0;i<y.length;i++)
     href[4]=href[0].replace(/coursehome/,"homework");
     href[5]=href[0].replace(/coursehome/,"groupLearning");
     href[6]=href[0].replace(/coursehome/,"forum");
-    for (var j=1;j<7;j++)
-		$(course).find("a:eq("+j.toString()+")").attr("href",href[j]);
 
- 
-   
-	//new makeMenu(course,i);
+    for (var j=1;j<7;j++)
+    {
+		$(course).find("a:eq("+j.toString()+")").attr("href",href[j]);
+		$(course).find("a:eq("+j.toString()+")").attr("target",'iframe_a');
+	}
+
 	$(course).mouseover((function(course_){ 
 	return function(){$(course_).children("ul:eq(0)").css("display","block");};
 	})(course)); 
@@ -43,8 +62,6 @@ for(var i=0;i<y.length;i++)
 	$(course).mouseout((function(course_){ 
 	return function(){$(course_).children("ul:eq(0)").css("display","none");};
 	})(course)); 
-	
+	//course.attr("width","50px");
 }
-$(".menu li").css('width','80px');
-$(".menu li").css('background','#CCC');
-$(".menu a:hover").css('background','#999');
+
