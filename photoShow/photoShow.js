f59bf5bf77d722f95c18983da2c74df6bcb3fce2
Ontree <wangyicmh@163.com>
2014-07-11 00:52:17 +0800

    no message

--- photoShow/photoShow.js
+++ photoShow/photoShow.js
@@ -1,5 +1,22 @@
-function loadXMLDoc(st){
-	var xmlhttp;
+var obj;
+
+function showPhoto(){
+	$('#mainPhoto').empty();
+	for (var i=1; i<=obj.number;i++ ){
+		var p =$("<div>");
+		p.addClass("Photo");
+		if (i !=1){
+			p.css("display","none");
+		}
+		//p.attr("id",i.toString());
+		var im=$("<img>");
+		im.attr("src",obj.PhotoSrc[i]);
+		p.append(im);
+		$('#mainPhoto').append(p);
+	}
+}
+function loadPhoto(st){
+	var xmlhttp, response;
 	if (window.XMLHttpRequest){
   		xmlhttp=new XMLHttpRequest();
   	}
@@ -8,19 +25,19 @@ function loadXMLDoc(st){
   	}
 	xmlhttp.onreadystatechange=function(){
   		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-    		return (xmlhttp.responseText);
+    		obj=eval("("+xmlhttp.responseText+")");
+    		showPhoto();
     	}
   	}
 	xmlhttp.open("GET",st,true);
 	xmlhttp.send();
-
 }
 
 $('.seriesPreviewItem').click((function(){
-	return function(){//$('.shade').css("display","block");
-	$('.shade').css('opacity',0.772);
-	$('#mainShow').css("display","block");
-	var obj=JSON.parse(loadXMLDoc("series_1.json"));
-	console.log(obj);
+	return function(){
+		$('.shade').css("display","block");
+		$('.shade').css('opacity',0.772);
+		$('#mainShow').css("display","block");
+		loadPhoto("series_1.json");
 	};
 })())
